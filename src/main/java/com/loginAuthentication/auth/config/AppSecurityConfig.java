d0ae0aaca6649f9ead9d8377556dad49de9acdaf
Olaoluwa22 <olaoluwaoni22@gmail.com>
2024-01-05 00:01:23 +0100

    Implemented a custom UserDetailsService, so as to change the default login credentials set by Spring Security

---
+++ src/main/java/com/loginAuthentication/auth/config/AppSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.loginAuthentication.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppSecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var userDetailsService = new InMemoryUserDetailsManager();
+        var userDetails = User.withUsername("admin")
+                .password("admin")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(userDetails);
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
