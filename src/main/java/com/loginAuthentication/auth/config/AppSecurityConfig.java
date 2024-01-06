4e0ca61ecd9edb9b67311416abc59d345d5eca23
Olaoluwa22 <olaoluwaoni22@gmail.com>
2024-01-06 13:23:49 +0100

    Modified the SecurityFilterChain

--- src/main/java/com/loginAuthentication/auth/config/AppSecurityConfig.java
+++ src/main/java/com/loginAuthentication/auth/config/AppSecurityConfig.java
@@ -2,19 +2,31 @@ package com.loginAuthentication.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppSecurityConfig {
     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        //authorization
+
+        return http.build();
+    }
+    @Bean
     public UserDetailsService userDetailsService(){
         var userDetailsService = new InMemoryUserDetailsManager();
-        var userDetails = User.withUsername("admin")
-                .password("admin")
+        var userDetails = User.withUsername("username")
+                .password("password")
                 .authorities("read")
                 .build();
         userDetailsService.createUser(userDetails);
